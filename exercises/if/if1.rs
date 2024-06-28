ac9604c2e2206cc7ece9d9444edf9c57cc348e67
nada-r <nadarzer@gmail.com>
2024-06-28 23:42:48 +0200

    ended functions

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -5,6 +5,10 @@
 // I AM NOT DONE
 
 pub fn bigger(a: i32, b: i32) -> i32 {
+    if a > b {
+        return a;
+    }
+    return b;
     // Complete this function to return the bigger number!
     // Do not use:
     // - another function call
